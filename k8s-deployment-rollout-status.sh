9bf50561a4faf7fb9e3134560a5aba4e895960a8
younesssmz <yyyymmzz@outlook.com>
2023-01-10 20:49:32 +0100

    Update k8s-deployment-rollout-status.sh

--- k8s-deployment-rollout-status.sh
+++ k8s-deployment-rollout-status.sh
@@ -2,7 +2,7 @@
 
 #k8s-deployment-rollout-status.sh
 
-if [[ $(kubectl -n default rollout status deploy ${deploymentName} --timeout 5s) != *"successfully rolled out"* ]];
+if [[ $(kubectl -n default rollout status deployment/${deploymentName} --timeout 5s) != *"successfully rolled out"* ]];
 then
     echo "Deployment ${deploymentName} Rollout has Failed"
     kubectl -n default rollout undo deploy ${deploymentName}
